9252d3b99b67be0921d831ca3c74ca2bb7d0ac20
Saksham Bhutani <sakshambhutani2001@gmail.com>
2026-05-31 02:41:08 -0400

    Initial Commit

---
+++ PulseLoop/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PulseLoop
+//
+//  Created by Saksham Bhutani on 5/31/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ PulseLoop/PulseLoopApp.swift
@@ -0,0 +1,32 @@
+//
+//  PulseLoopApp.swift
+//  PulseLoop
+//
+//  Created by Saksham Bhutani on 5/31/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PulseLoopApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
